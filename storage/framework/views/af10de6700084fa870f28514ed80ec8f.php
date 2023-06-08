ebea17f1811fa283e55911fd64dbc2bdd459a119
Prasastio Giri Wicaksono <133931365+prasastiogiri@users.noreply.github.com>
2023-06-08 12:47:10 +0700

    Add files via upload

---
+++ storage/framework/views/af10de6700084fa870f28514ed80ec8f.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar Controller & View pada Laravel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <?php echo app('Illuminate\Foundation\Vite')('resources/sass/app.scss'); ?>
+</head>
+
+<body>
+    <div class="container text-center my-5">
+        <h1 class="mb-4">Belajar Controller & View pada Laravel</h1>
+        
+        <img class="rounded mx-auto d-block" style="width: 200px" src="<?php echo e(Vite::asset('resources/images/laravel.png')); ?>" alt="image">
+        <div class="col-md-2 offset-md-5 mt-4">
+            <div class="d-grid gap-2">
+                <a class="btn btn-dark" href="<?php echo e(url('/home')); ?>">Home</a>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <?php echo app('Illuminate\Foundation\Vite')('resources/js/app.js'); ?>
+</body>
+
+</html>
+<?php /**PATH D:\Kuliah\ITTS\Semester 6\PF\Modul-04\resources\views/welcome.blade.php ENDPATH**/ ?>
